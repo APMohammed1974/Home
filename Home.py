32e07cc2f0ea15d5129119bbc54b60c0e6826534
MMA <muhammed07701445804@gmail.com>
2024-11-03 20:10:12 +0300

    Web app for todo list update

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import functions
 
 
 todos=functions.read_todos()
+st.set_page_config(layout="wide")
 def add_todo():
     todo=st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -10,7 +11,10 @@ def add_todo():
 
 st.title("Todo app")
 st.subheader("This is my first app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b> productivity</b>", unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add new todo....", on_change=add_todo, key="new_todo")
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -18,4 +22,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-st.text_input(label="", placeholder="Add new todo....", on_change=add_todo, key="new_todo")
